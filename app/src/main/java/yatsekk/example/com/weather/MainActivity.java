c6c6e41e4c4c1c7e5dc8cdb8a79ccc2b7d41bca2
YatsekK <34369637+YatsekK@users.noreply.github.com>
2018-12-02 11:34:00 +0300

    Lifecycle toasted

--- app/src/main/java/yatsekk/example/com/weather/MainActivity.java
+++ app/src/main/java/yatsekk/example/com/weather/MainActivity.java
@@ -3,6 +3,7 @@ package yatsekk.example.com.weather;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,53 +15,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: ");
+        makeToast("onCreate");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Log.d(TAG, "onStart: ");
+        makeToast("onStart");
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.d(TAG, "onRestoreInstanceState: ");
+        makeToast("onRestoreInstanceState");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         Log.d(TAG, "onResume: ");
+        makeToast("onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.d(TAG, "onPause: ");
+        makeToast("onPause");
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "onSaveInstanceState: ");
+        makeToast("onSaveInstanceState");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.d(TAG, "onStop: ");
+        makeToast("onStop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart: ");
+        makeToast("onRestart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: ");
+        makeToast("onDestroy");
     }
+
+    private void makeToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
 }
